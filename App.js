eacb09cdc81945fe471772e4d59f79e426dd73e6
gmagnere <103102555+gmagnere@users.noreply.github.com>
2022-06-14 16:30:04 +0200

    exo 3 random par nom

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import {
     Platform,
     Text,
@@ -16,9 +18,12 @@ import {useEffect, useState} from "react";
 
 
 export default function App() {
+    const Tab = createBottomTabNavigator();
     const [quote, setQuote] = useState([]);
     const [quoteList, setQuoteList] = useState([]);
     const [number, setNumber] = useState([]);
+    const [name, setName] = useState([]);
+    const [quotesByName, setQuotesByName] = useState([]);
     const LeftContent = props => <Avatar.Icon {...props}/>
 
 
@@ -41,6 +46,17 @@ export default function App() {
         )
   }
 
+    const getQuotesByName = () => {
+        fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=15&character=${name}`)
+            .then(response => response.json())
+            .then(data => {
+                    console.log(data);
+                    setQuotesByName(data);
+                }
+            )
+    }
+
+
     const onPressQuote= () => {
         //console.log("test")
         getANumberOfQuote()
@@ -54,6 +70,24 @@ export default function App() {
                 style={styles.input}
                 onChangeText={setNumber}
                 value={number}
+                placeholder='Entrer ici'
+            />
+        )
+    }
+
+    const onPressQuoteByName= () => {
+        //console.log("test")
+        getQuotesByName()
+
+        //console.log(goalList)
+    };
+
+    const inputAddQuoteByName = () => {
+        return (
+            <TextInput
+                style={styles.input}
+                onChangeText={setName}
+                value={name}
                 placeholder='Entrer ici'
             />
         )
@@ -79,6 +113,12 @@ export default function App() {
     const Quotes = ({ quoteList }) => (
         <View style={styles.item}>
             <Text style={styles.title}>{quoteList}</Text>
+        </View>
+    )
+
+    const QuotesByName = ({ quotesByName }) => (
+        <View style={styles.item}>
+            <Text style={styles.title}>{quotesByName}</Text>
         </View>
     )
 
@@ -109,23 +149,70 @@ export default function App() {
         </View>
     );
 
+    const renderQuotesByName = ({ item }) => (
+        <View >
+            <Card>
+                <Card.Title title={item.character} subtitle="" left={LeftContent} />
+                <Card.Content>
+                    <QuotesByName quotesByName={item.quote}></QuotesByName>
+                </Card.Content>
+            </Card>
+        </View>
+    );
+
+    const SimpsonByName = () => {
+
+        return (
+        <View>
+            {inputAddQuoteByName()}
+            <Button
+                onPress={onPressQuoteByName}
+            >Charger des citations avec le nom d'un personnage</Button>
+            <FlatList
+                data={quotesByName}
+                renderItem={renderQuotesByName}
+                keyExtractor={item => item.dt}
+
+            />
+        </View>
+        )
+    }
+
+    const SimpsonByNumber = () => {
+        return (
+            <View>
+                {inputAddQuote()}
+                <Button
+                    onPress={onPressQuote}
+                >Charger des citations</Button>
+                <FlatList
+                    data={quoteList}
+                    renderItem={renderItemQuoteList}
+                    keyExtractor={item => item.dt}
+                />
+            </View>
+        )
+    }
+
+    const SimpsonQuotes = () => {
+        return (
+            <View>
+                <FlatList
+                    data={quote}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.dt}
+                />
+            </View>
+        )
+    }
 
   return (
     <View style={styles.container}>
-        <FlatList
-            data={quote}
-            renderItem={renderItem}
-            keyExtractor={item => item.dt}
-        />
-        {inputAddQuote()}
-        <Button
-            onPress={onPressQuote}
-        >Charger des citations</Button>
-        <FlatList
-            data={quoteList}
-            renderItem={renderItemQuoteList}
-            keyExtractor={item => item.dt}
-        />
+        {SimpsonQuotes()}
+        {SimpsonByNumber()}
+        {SimpsonByName()}
+
+
     </View>
   );
 }
