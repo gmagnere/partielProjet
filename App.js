7b16bed6789ddf60c7a10dc7377ebe71535db432
gmagnere <103102555+gmagnere@users.noreply.github.com>
2022-06-14 13:36:32 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, Text, View, StyleSheet, FlatList, ActivityIndicator, Image, ImageBackground } from 'react-native';
+import {useEffect} from "react";
+
 
 export default function App() {
+  const [quote, setQuote] = useState([]);
+
+  const getQuote = () => {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
+        .then(response => response.json())
+        .then(data => {
+              console.log(data);
+              setQuote(data);
+            }
+        )
+  }
+  useEffect(() => {
+
+    (async () => {
+      getQuote()
+    })();
+
+  }, []);
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
